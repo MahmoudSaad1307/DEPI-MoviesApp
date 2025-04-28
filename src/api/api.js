97ff29583cd97424ebd1b8577b183a1c97854b8c
Malik1307 <mahmoud2004saad@gmail.com>
2025-04-28 13:57:15 +0300

    register N login endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -15,27 +15,4 @@ export const loginUser = (email, password) => {
   return api.post('/users/login', { email, password });
 };
 
-export const getReviewsByMovieId = (movieId) => {
-  return api.get(`/reviews/movie/${movieId}`);
-};
 
-export const createReview = (reviewData) => {
-  return api.post('/reviews', reviewData);
-};
-
-export const getUserList = (userId) => {
-  return api.get(`/userlists/${userId}`);
-};
-
-export const updateUserList = (userId, movieId, action) => {
-  return api.put(`/userlists/${userId}`, { movieId, action });
-};
-
-// Get user profile (requires token)
-export const getProfile = (token) => {
-  return api.get('/users/profile', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
