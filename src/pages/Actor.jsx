132f6ba8bb25dee5c2989f4d2973036c8d3ad81f
Mahmoud Saad <mahmoud2004saad@gmail.com>
2026-01-14 08:55:40 +0200

    Rename actor.css to Actor.css for consistency

--- src/pages/Actor.jsx
+++ src/pages/Actor.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./actor.css";
+import "./Actor.css";
 import { useParams, Link } from "react-router-dom";
 import Pagination, {
   paginate,
