dc9e11758464d788aa9dcdf60385c28005d86a4f
Mahmoud Saad <mahmoud2004saad@gmail.com>
2026-01-14 08:53:50 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import TopPicks from "./../components/topPicks";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./HomePage.css";
+import "./Homepage.css";
 import './CarouselFixes.css'
 import {
   BASE_URL,
